70b84eddd906f25dd7e661cec90108d890a7083d
patkanybazis <raad23tt@student.ju.se>
2025-09-19 22:48:01 +0200

    lab 3

--- server.js
+++ server.js
@@ -4,52 +4,94 @@ const app = express();
 const sqlite3 = require("sqlite3");
 const dbFile = "my-project-db.sqlite3.db";
 const db = new sqlite3.Database(dbFile);
+const { engine } = require("express-handlebars");
 
-app.use(express.static(__dirname)); //by mr. gpt cus the css was not working
+//HANDLEBARS
+app.engine("handlebars", engine());
+app.set("view engine", "handlebars");
+app.set("views", "./views");
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
+app.use(express.static("public"));
+
+// MODEL //missing pictures
+const projects = [
+  {
+    id: "1",
+    name: "Counting people with a camera",
+    type: "Research",
+    desc: "The purpose of this project is to count people passing through a corridor and to know how many are in the room at a certain time.",
+    year: 2022,
+    dev: "Python and OpenCV (Computer vision) library",
+    url: "/img/counting.png",
+  },
+  {
+    id: "2",
+    name: "Visualisation of 3D medical images",
+    type: "Research",
+    desc: "The project makes a 3D model of the analysis of the body of a person and displays the detected health problems. It is useful for doctors to view in 3D their patients and the evolution of a disease.",
+    year: 2012,
+    url: "/img/medical.png",
+  },
+  {
+    id: "3",
+    name: "Multiple questions system",
+    type: "Teaching",
+    desc: "During the lockdowns in France, this project was useful to test the students online with a Quizz system.",
+    year: 2021,
+    url: "/img/qcm07.png",
+  },
+  {
+    id: "4",
+    name: "Image comparison with the Local Dissmilarity Map",
+    desc: "The project is about finding and quantifying the differences between two images of the same size. The applications were numerous: satellite imaging, medical imaging,...",
+    year: 2020,
+    type: "Research",
+    url: "/img/diaw02.png",
+  },
+  {
+    id: "5",
+    name: "Management system for students' internships",
+    desc: "This project was about the creation of a database to manage the students' internships.",
+    year: 2012,
+    type: "Teaching",
+    url: "/img/management.png",
+  },
+  {
+    id: "6",
+    name: "Magnetic Resonance Spectroscopy",
+    desc: "Analysis of signals and images from Magnetic Resonance Spectroscopy and Imaging.",
+    year: 2013,
+    type: "Research",
+    url: "/img/yu00.png",
+  },
+  {
+    id: "7",
+    name: "Signal Analysis for Detection of Epileptic Deseases",
+    desc: "This project was about the detection of epileptic problems in signals.",
+    year: 2019,
+    type: "research",
+    url: "/img/youssef00.png",
+  },
+];
+
+app.get("/", function (req, res) {
+  //res.send("Hello World");
+  res.render("home.handlebars");
 });
 
-app.get("/cv", function (req, res) {
-  res.sendFile(
-    __dirname +
-      "C:\\Users\\Adri\\OneDrive - Jonkoping University\\Desktop\\project-WDF\\views\\mycv-02.html"
-  );
+app.get("/about", (req, res) => {
+  res.render("cv.handlebars");
 });
 
-app.get("/rawpersons", function (req, res) {
-  db.all("SELECT * FROM Person", function (err, rawPersons) {
-    if (err) {
-      console.log("error: ", err);
-    } else {
-      console.log("Data found, sending it back to the client...");
-      res.send(rawPersons);
-    }
-  });
+app.get("/contact", (req, res) => {
+  res.render("contact.handlebars");
 });
 
-app.get("/listpersons", function (req, res) {
-  db.all("SELECT * FROM Person", function (err, rawPersons) {
-    if (err) {
-      console.log("Error: ", +err);
-    } else {
-      listPersonsHTML = "<ul>";
-      rawPersons.forEach(function (onePerson) {
-        listPersonsHTML += "<li>";
-        listPersonsHTML += `${onePerson.fname},`;
-        listPersonsHTML += `${onePerson.lname},`;
-        listPersonsHTML += `${onePerson.age},`;
-        listPersonsHTML += `${onePerson.email},`;
-        listPersonsHTML += "</li>";
-      });
-      listPersonsHTML += "</ul>";
-      res.send(listPersonsHTML);
-    }
-  });
+app.get("/projects", (req, res) => {
+  const model = { projects: projects };
+  res.render("projects.handlebars", model);
 });
 
 app.listen(port, () => {
   console.log(`Server up and running on http://localhost:${port}...`);
 });
-//testing source control
